7ca0da6a338477be3707e8c31eb7f4d2761c62b0
MY PC <MY PC@HOME-UEFI1TB.local>
2018-10-02 11:36:46 +0700

    Project Gandy Christian Situmorang 10116228 PBO6

---
+++ src/pbo6/pkg10116228/latihan18/kelipatan/PBO610116228Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg10116228.latihan18.kelipatan;
+
+/**
+ *
+ * @@author 
+ * NAMA : Gandy Christian Situmorang
+ * KELAS: PBO-6
+ * NIM  : 10116338
+ * Deskripsi Program : Program ini berisi program perulangan yang menampilkan 
+ * kelipatan angka 3.5 secara ascending
+ */
+public class PBO610116228Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i);
+    }
+    
+}
